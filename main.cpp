8a4661d0fec4fe2a11e2d7005df8ed9d2c1f1d55
Tommaso Scorza <91656025+scovza@users.noreply.github.com>
2021-11-09 20:08:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+
+    int n = 0;
+
+    cin >> n;
+
+
+        int a = 1;
+        int b = 1;
+        int c = 0;
+
+        cout << a << endl;
+        cout << b << endl;
+
+        for (int i=3; i <= n; i++){
+
+            c=a+b;
+            a=b;
+            b=c;
+
+            if(c<=n){
+
+                cout << c << endl;
+            }else {
+
+                break;
+            }
+        }
+
+
+    return 0;
 }
